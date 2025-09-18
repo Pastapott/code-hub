25bd71d245749040da689c21a0dcd6556e238d21
CallumIsh <caisherwood@gmail.com>
2025-09-18 22:15:39 +0100

    Just testing push and window made

---
+++ chessmain.py
@@ -0,0 +1,16 @@
+#imports tkinter into file
+import tkinter as tk
+
+#creates the window 
+root = tk.Tk()
+root.title("Chess")
+
+#frame to display board
+board_frame = tk.Frame(root)
+board_frame.pack()
+
+
+
+#runs the tkinter
+root.mainloop()
+
